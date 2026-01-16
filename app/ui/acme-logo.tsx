2aff517708e603fa73109ff607c75466b98ad3ba
Ikhsan Shah Pohan <ihksanpohan@gmail.com>
2026-01-16 15:39:38 +0700

    first commit

--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -1,5 +1,5 @@
-import { GlobeAltIcon } from '@heroicons/react/24/outline';
-import { lusitana } from '@/app/ui/fonts';
+import { GlobeAltIcon } from "@heroicons/react/24/outline";
+import { lusitana } from "./fonts";
 
 export default function AcmeLogo() {
   return (
@@ -8,6 +8,6 @@ export default function AcmeLogo() {
     >
       <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
       <p className="text-[44px]">Acme</p>
-    </div>
+    </div>  
   );
 }
